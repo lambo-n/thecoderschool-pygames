74e37ecd719043967d57a8c8002507f646b8ca1f
lambo <nathanyflam@gmail.com>
2026-04-23 19:31:38 -0700

    lucas 4/23

---
+++ lucasCollector.py
@@ -0,0 +1,67 @@
+# Example file showing a circle moving on screen
+import pygame
+import random
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720), pygame.FULLSCREEN)
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+spirit_pos = pygame.Vector2(500, 200)
+
+freddyImage = pygame.image.load("assets/robotVillain.png").convert_alpha()
+freddyImage = pygame.transform.scale(freddyImage, (100, 100))
+
+spiritImage = pygame.image.load("assets/cryingChild.png").convert_alpha()
+spiritImage = pygame.transform.scale(spiritImage, (50, 50))
+
+
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("brown")
+    
+    freddyRect = freddyImage.get_rect(center=player_pos)
+    spiritRect = spiritImage.get_rect(center=spirit_pos)
+    
+    if freddyRect.colliderect(spiritRect):
+        randomNumX = random.randint(0, 1280)
+        randomNumY = random.randint(0, 720)
+        
+        
+        SpiritCount += 1
+        print(SpiritCount)
+        
+        spirit_pos = pygame.Vector2(randomNumX, randomNumY)
+    
+    screen.blit(freddyImage, player_pos)
+    screen.blit(spiritImage, spirit_pos)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
