f329f4b3f4daa42a4882aa1cc6fdf4c3486e2d48
lambo-n <86932850+lambo-n@users.noreply.github.com>
2026-06-05 15:49:56 -0700

    Refactor ObbyPlatform class for code style consistency

--- davidPlatform.py
+++ davidPlatform.py
@@ -1,7 +1,7 @@
 import pygame
 
 class ObbyPlatform:
-    def __init__(self, posx, posy, width, height, color):
+    def __init__ (self, posx, posy, width, height, color):
         self.posx = posx
         self.posy = posy
         self.width = width
@@ -10,13 +10,32 @@ class ObbyPlatform:
         self.rect = pygame.Rect(self.posx, self.posy, self.width, self.height)
 
     @property
-    def top(self):    return self.rect.top
+    def top(self): return self.rect.top
     @property
     def bottom(self): return self.rect.bottom
     @property
-    def left(self):   return self.rect.left
+    def left(self): return self.rect.left
     @property
-    def right(self):  return self.rect.right
+    def right(self): return self.rect.right
 
-    def update(self, screen):
+    def update(self,screen):
         pygame.draw.rect(screen, self.color, self.rect)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
