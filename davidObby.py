f181a1a4beeb238804a15047008a61ae1c7bdc77
lambo-n <86932850+lambo-n@users.noreply.github.com>
2026-06-12 16:31:36 -0700

    Refactor platform initialization and player image

--- davidObby.py
+++ davidObby.py
@@ -1,6 +1,6 @@
 # Example file showing a circle moving on screen
 import pygame
-from davidPlatform import ObbyPlatform
+from obby_platform import ObbyPlatform
 
 # pygame setup
 pygame.init()
@@ -11,39 +11,37 @@ dt = 0
 canJump = False
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+playerRect = pygame.Rect(player_pos.x - 32, player_pos.y - 32, 32,32)
 
-playerImage = pygame.image.load("assets/blasterboy.png").convert_alpha()
-playerImage = pygame.transform.scale(playerImage, (64, 64))
+playerImage = pygame.image.load("assets/jumpboy.png").convert_alpha()
+playerImage = pygame.transform.scale(playerImage, (32, 32))
 gravity = 0
 
-# LEVEL 1
-platform1 = ObbyPlatform(0,650,1280,100,"blue")
-platform2 = ObbyPlatform(50,500,300,50,"black")
-platform3 = ObbyPlatform(200,325,300,50,"black")
-platform4 = ObbyPlatform(500,200,300,50,"black")
+# lvl 1 platforms
+platform1lvl1 = ObbyPlatform(0,650,1280,100,"blue")
+platform2lvl1 = ObbyPlatform(50,500,300,50,"black")
+platform3lvl1 = ObbyPlatform(200,325,300,50,"black")
+platform4lvl1 = ObbyPlatform(500,200,300,50,"black")
 
-lvl1List = [platform1,platform2,platform3,platform4]
+lvl1List = [platform1lvl1,platform2lvl1,platform3lvl1,platform4lvl1]
 
-# LEVEL 2
-
-
-lvl2List = [platform4]
-
-
-
+# lvl 2 platforms
+platform1lvl2 = ObbyPlatform(0,650,1280,100,"blue")
+platform2lvl2 = ObbyPlatform(400,400,1,1,"black")
+platform3lvl2 = ObbyPlatform(200,600,1,1,"black")
+platform4lvl2 = ObbyPlatform(700,200,1,1,"black")
 
 
+lvl2List = [platform1lvl2, platform2lvl2, platform3lvl2, platform4lvl2]
 
 
-levels = [lvl1List, lvl2List]
+levels = [lvl1List,lvl2List]
 
-currentPlatformList = lvl2List
 
-currentLevel = 1
-
+currentLvl = 2
+currentPlatformList = lvl1List
 
 escapeRect = pygame.Rect(1000,100,50,50)
-
 
 while running:
     # poll for events
@@ -52,20 +50,32 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("white")
-    
-    for platform in currentPlatformList:
-        platform.update(screen)
 
-    pygame.draw.rect(screen,"gold",escapeRect)
+    # MOVEMENT
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w] and canJump == True:
+        player_pos.y -= 20
+        gravity = -600 * dt
+        canJump = False
+    if keys[pygame.K_s]:
+        pass
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
 
+
+    # GRAVITY
     gravity += 12 * dt
     player_pos.y += gravity
     
+    
+    # update player hitbox
     playerRect = pygame.Rect(player_pos.x - 32, player_pos.y - 32, 64,64)
 
-    #PLATFORM PHYSICS
+
+    # COLLISION CHECKS
+    # platform physics
     canJump = False
     for platform in currentPlatformList:
         if playerRect.colliderect(platform):
@@ -93,24 +103,25 @@ while running:
             player_pos.y = playerRect.centery
 
 
+    # door collision check
+    if escapeRect.colliderect(playerRect):
+        gravity = 0
+        player_pos.x = 100
+        player_pos.y = 600
+        currentLvl += 1
 
 
 
-    print(gravity)
+    # PRINT STUFF ON SCREEN
+    screen.fill("white")
 
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w] and canJump == True:
-        player_pos.y -= 20
-        gravity = -600 * dt
-        canJump = False
-    if keys[pygame.K_s]:
-        pass
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    # print/move platforms
+    currentPlatformList = levels[currentLvl - 1]
+    for platform in currentPlatformList:
+        platform.update(screen)
 
+    pygame.draw.rect(screen,"gold",escapeRect)
     screen.blit(playerImage,playerRect)
 
     # flip() the display to put your work on screen
