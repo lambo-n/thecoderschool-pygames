4bb052b5c30df9be189165ce3a7dfca97357e5aa
lambo-n <86932850+lambo-n@users.noreply.github.com>
2026-06-13 10:57:32 -0700

    6/13

--- ethanPong.py
+++ ethanPong.py
@@ -1,4 +1,3 @@
-# Example file showing a circle moving on screen
 import pygame
 
 # pygame setup
@@ -13,17 +12,14 @@ WIDTH = screen.get_width()
 
 player1_y = HEIGHT / 2
 player2_y = HEIGHT / 2
-circle_pos = pygame.Vector2(600, 300)
-
-speedx = 300 
+circle_pos = pygame.Vector2(600,300)
+speedx = 300
 speedy = 300
+points1 = 0
+points2 = 0
 
 
 
-
-
-
-# game loop
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -34,47 +30,91 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
     
-    midline = pygame.draw.line(screen, "white", (600, 0), (600, 600), 20)
-    
-    circle = pygame.draw.circle(screen, "white", circle_pos, 15)
-    
+
+    # moving stuff
     circle_pos.x += speedx * dt
     circle_pos.y += speedy * dt
-    
-    circleHitBox = pygame.Rect(circle_pos.x-7.5, circle_pos.y-7.5, 15, 15)
-    
-    if circleHitBox.right >= WIDTH or circleHitBox.left <= 0:
-        speedx *= -1
-        
-    if circleHitBox.top <= 0 or circleHitBox.bottom >= HEIGHT:
-        speedy *= -1
-        
-        
-    if pong1.colliderect(circleHitBox):
-        speedx *= -1
-
-    pong1 = pygame.Rect(100, player1_y, 25, 115)
-    
-    pong2 = pygame.Rect(1100, player2_y, 25, 115)
-
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w] and player1_y >= 0: 
-        player1_y -= 300 * dt
-    if keys[pygame.K_s] and player1_y <= HEIGHT-115:
-        player1_y += 300 * dt
-        
+    # player 1
+    if keys[pygame.K_w] and player1_y >= 0:
+        player1_y -= 390 * dt
+    if keys[pygame.K_s] and player1_y <= 485:
+        player1_y += 390 * dt
+
+    # player 2
     if keys[pygame.K_UP] and player2_y >= 0:
-        player2_y -= 300 * dt
-    if keys[pygame.K_DOWN] and player2_y <= HEIGHT-115:
-        player2_y += 300 * dt 
+        player2_y -= 390 * dt
+    if keys[pygame.K_DOWN] and player2_y <= 485:
+        player2_y += 390 * dt
 
-    # draw stuff
-    pygame.draw.rect(screen, "blue", pong2)
+  
+    # hitboxes
+    circleHitBox = pygame.Rect(circle_pos.x-7.5,circle_pos.y - 7.5 ,15,15)
+    pong1 = pygame.Rect(100,player1_y,25,115)
+    pong2 =  pygame.Rect(1100,player2_y,25,115)
+    
+    
+   
+    # collision checks
+
+
+    if circleHitBox.right >= WIDTH or circleHitBox.left <= 0:
+      
+        print("bounce")
+
+    if circleHitBox.top <= 0 or circleHitBox.bottom >= 600:
+        speedy *= -1
+        print("BOUNCE")
+    
+    if pong1.colliderect(circleHitBox):
+        speedx *= -1
+        circle_pos.x = pong1.right + 7.5
+
+    if pong2.colliderect(circleHitBox):
+        speedx *= -1
+        circle_pos.x = pong2.left - 7.5
+
+    if pong1.colliderect(circleHitBox) or pong2.colliderect(circleHitBox):
+        speedx *= 1.05
+    # check
+    if circle_pos.x < 0: 
+        points2 += 1
+        print("Points2")
+        circle_pos.x = 600
+        circle_pos.y = 300
+        speedx = 1
+    if circle_pos.x > 1200:
+        points1 += 1
+        print("Points1")
+        circle_pos.x = 600
+        circle_pos.y = 300
+        speedx = 1
+
+    font = pygame.font.SysFont("Arial", 40)
+    livesText = font.render(f"{points1} - {points2}", True, "white")
+    screen.blit(livesText, (560,0))
+    
+    
+    if points1 == 5 or points2 ==5:
+        running = False
+        
+        
+        
+    # if points1 == 5:
+    #     screen.fill("red")
+
+    # elif points2 ==5:
+    #     screen.fill("blue")
+# pong1 = red pong2 = blue
+
+
+
+    # drawing stuff    
+    midline = pygame.draw.line(screen, "grey",(600,0), (600, 600), 20)
+    circle = pygame.draw.circle(screen,"white",circle_pos, 15)
     pygame.draw.rect(screen, "red", pong1)
-
-
-
+    pygame.draw.rect( screen, "blue", pong2)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
