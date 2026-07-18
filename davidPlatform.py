9b96e36d4aa4f24ad66b06c9499d575055735789
lambo-n <86932850+lambo-n@users.noreply.github.com>
2026-07-17 17:06:28 -0700

    Implement new platform classes with collision detection
    Added Teleporter, Killblock, and Escape classes with updated collision handling for player interactions.

--- davidPlatform.py
+++ davidPlatform.py
@@ -8,6 +8,8 @@ class ObbyPlatform:
         self.height = height
         self.color = color
         self.rect = pygame.Rect(self.posx, self.posy, self.width, self.height)
+        
+
 
     @property
     def top(self): return self.rect.top
@@ -18,17 +20,59 @@ class ObbyPlatform:
     @property
     def right(self): return self.rect.right
 
-    def update(self,screen):
+    def update(self,screen, player_pos ,playerHitbox):
         pygame.draw.rect(screen, self.color, self.rect)
+        return False
+
+
         
 
 
+class Teleporter(ObbyPlatform):
+    def __init__(self, posx, posy, width, height, color,teleportX,teleportY):
+
+        super().__init__(posx, posy, width, height, color)
+
+        self.teleportX = teleportX
+        self.teleportY = teleportY
+        
+
+    def update(self,screen,player_pos,playerHitbox):
+        super().update(screen, player_pos ,playerHitbox)
+  
+        if playerHitbox.colliderect(self.rect):
+            player_pos.x = self.teleportX
+            player_pos.y = self.teleportY
 
 
 
 
+class Killblock(ObbyPlatform):
+    def __init__(self, posx, posy, width, height, color):
+
+        super().__init__(posx, posy, width, height, color)
+    
+    def update(self,screen,player_pos,playerHitbox):
+            super().update(screen, player_pos ,playerHitbox)
+    
+            return playerHitbox.colliderect(self.rect)
+                
+
+class Escape(ObbyPlatform):
 
 
+    def __init__(self, posx, posy, width, height, color,escapeX,escapeY):
+
+        super().__init__(posx, posy, width, height, color)
+
+        self.escapeX = escapeX
+        self.escapeY = escapeY
+
+    def update(self,screen,player_pos,playerHitbox):
+            super().update(screen, player_pos ,playerHitbox)
+    
+            if playerHitbox.colliderect(self.rect):
+                print("escape")
 
 
 
