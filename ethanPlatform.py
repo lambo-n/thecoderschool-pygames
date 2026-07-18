07f47ed47032be025452df99dfe70f55f0edd514
lambo <nathanyflam@gmail.com>
2026-07-18 11:39:50 -0700

    ethan 7/18

---
+++ ethanPlatform.py
@@ -0,0 +1,32 @@
+import pygame
+
+class CustomPlatform:
+    def __init__ (self, posx, posy, width, height, color):
+        self.posx = posx
+        self.posy = posy
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rect = pygame.Rect(self.posx, self.posy, self.width, self.height)
+        
+      
+        
+
+
+    @property
+    def top(self): return self.rect.top
+    @property
+    def bottom(self): return self.rect.bottom
+    @property
+    def left(self): return self.rect.left
+    @property
+    def right(self): return self.rect.right
+
+    def update(self,screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+
+
+
+        
+
+
---
+++ ethanPlatformer.py
@@ -0,0 +1,111 @@
+# Example file showing a circle moving on screen
+import random
+
+import pygame
+from platformTemplate import *
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+gravity = 0
+canJump = False
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+
+# xpos, ypos, xwidth, yheight
+platformGround = CustomPlatform(0, 600, 1280, 20, "white")
+platform1 = CustomPlatform(100, 250, 150, 20, "white")
+platform2 = CustomPlatform(400, 450, 150, 20, "white")
+platform3 = CustomPlatform(700, 150, 150, 20, "white")
+platform4 = CustomPlatform(100, 350, 150, 20, "white")
+
+
+platformList = [platformGround, platform1, platform2, platform3, platform4]
+
+
+
+
+font = pygame.font.SysFont(None, 40)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+ 
+        
+        
+    player_rect = pygame.Rect(player_pos.x - 20, player_pos.y - 20, 40, 40)
+    
+   
+    
+    gravity += 1000 * dt
+    player_pos.y += gravity * dt
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and canJump:
+        gravity = -600
+        canJump = False
+    if keys[pygame.K_LEFT]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_RIGHT]:
+        player_pos.x += 300 * dt
+
+    # Rebuild rect after movement so collisions use updated position
+    player_rect = pygame.Rect(player_pos.x - 20, player_pos.y - 20, 40, 40)
+
+    # Platform collisions using minimum overlap (MTV)
+    canJump = False
+    for platform in platformList:
+        if player_rect.colliderect(platform):
+            overlap_top    = player_rect.bottom - platform.top
+            overlap_bottom = platform.bottom - player_rect.top
+            overlap_left   = player_rect.right - platform.left
+            overlap_right  = platform.right - player_rect.left
+            overlap_y = min(overlap_top, overlap_bottom)
+            overlap_x = min(overlap_left, overlap_right)
+
+            if overlap_y <= overlap_x:
+                # Vertical collision
+                if gravity >= 0 and overlap_top <= overlap_bottom:
+                    player_rect.bottom = platform.top
+                    gravity = 0
+                    canJump = True
+                else:
+                    player_rect.top = platform.bottom
+                    gravity = 0
+            else:
+                # Horizontal collision
+                if overlap_left <= overlap_right:
+                    player_rect.right = platform.left
+                else:
+                    player_rect.left = platform.right
+            player_pos.x = player_rect.centerx
+            player_pos.y = player_rect.centery
+
+    player_rect.clamp_ip(screen.get_rect())
+    player_pos.x = player_rect.centerx
+    player_pos.y = player_rect.centery
+    
+    screen.fill("black")
+
+    for platform in platformList:
+        platform.update(screen)
+    
+    pygame.draw.rect(screen, "gold", player_rect)
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
