1cfcafa6478677f0f323a50f780548c8195fb11b
lambo-n <86932850+lambo-n@users.noreply.github.com>
2026-07-22 16:36:31 -0700

    Refactor CustomPlatform color assignment logic
    Refactored platform type handling to use a loop for color assignment based on type. Added new platform types and corresponding colors.

--- derrickCustomPlatform.py
+++ derrickCustomPlatform.py
@@ -1,8 +1,9 @@
 import pygame
+import random
 
-#TYPES:
-# normal - black
-# phase - purple
+# Types:
+    # Normal - Black
+    # Phase - Purple
 
 # A platform is just a pygame.Rect that also remembers how to draw itself
 # (its color) and what kind of platform it is (its type).
@@ -10,13 +11,42 @@ class CustomPlatform(pygame.Rect):
     def __init__(self, posx, posy, width, height, type="normal"):
         super().__init__(posx, posy, width, height)
         self.type = type
-        
-        if type == "normal":
-            self.color = "black"
-        elif type == "phase":
-            self.color = "purple"
-        elif type == "bounce":
-            self.color = "blue"
-        else:
-            self.color = "black"
-    
+        flag = True
+        while flag:
+            if type == "normal" or type == 0:
+                self.color = "#000000"
+                type = "normal"
+                flag = False
+            elif type == "phase" or type == 1:
+                self.color = "#AA4FFF"
+                self.type = "phase"
+                flag = False
+            elif type == "climb" or type == 2:
+                self.color = "#4FFF4F"
+                self.type = "climb"
+                flag = False
+            elif type == "fall" or type == 3:
+                self.color = "#FF4FFF"
+                self.type = "fall"
+                flag = False
+            elif type == "bounce" or type == 4:
+                self.color = "#80DFFF"
+                self.type = "bounce"
+                flag = False
+            elif type == "speed" or type == 5:
+                self.color = "#FF884F"
+                self.type = "speed"
+                flag = False
+            elif type == "danger" or type == 6:
+                self.color = "#F22525"
+                self.type = "danger"
+                flag = False
+            elif type == "random":
+                type = random.randint(0, 7)
+                self.color = "#FFFFFF"
+            # elif type == "crash":
+                # self.color = "#F22525"
+            else:
+                self.color = "#000000" # Black
+                flag = False
+            # print(type)
