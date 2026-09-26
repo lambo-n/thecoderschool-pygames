42e2480133f75d1fa136f9ec8c0a3eea04601612
lambo-n <86932850+lambo-n@users.noreply.github.com>
2026-09-26 10:07:13 -0700

    Refactor player controls and bullet mechanics
    Updated bullet cooldowns and player health values. Changed controls for jumping and movement to use 'W', 'A', 'D', and 'E'.

--- ethanPlatformer.py
+++ ethanPlatformer.py
@@ -1,12 +1,12 @@
 # Example file showing a circle moving on screen
 import random
-from ethanBullet import *
+from bullet import *
 import pygame
-from ethanPlatform import *
-from ethanLevels import *
-from ethanEnemies import*
-BULLET_COOLDOWN = .95
-
+from custom_platform import *
+from levels import *
+from enemies import*
+ENEMYBULLET_COOLDOWN = 1
+PLAYERBULLET_COOLDOWN = .2
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -15,21 +15,22 @@ running = True
 dt = 0
 gravity = 0
 canJump = False
-bulletcooldown = BULLET_COOLDOWN
-playerHealth = 10
+bulletcooldown = ENEMYBULLET_COOLDOWN
+playerHealth = 20
 player_pos = pygame.Vector2(300, 600)
-
+playerbulletcooldown = PLAYERBULLET_COOLDOWN
+PlayerBulletDamage = 6
 
 # xpos, ypos, xwidth, yheight
 
 
-currentLevel = 2
+currentLevel = 1
 platformList = levels[currentLevel-1]()
 gameWOn = False
 
 
-enemyBulletList = []
-playerBulletList = []
+Enemybulletlist = []
+playerbulletlist = []
 enemyList = enemies[currentLevel - 1]()
 
 font = pygame.font.SysFont(None, 40)
@@ -45,12 +46,18 @@ while running:
 
     # bullet loop
     bulletcooldown -= dt
+    playerbulletcooldown -= dt
+
     if bulletcooldown <0:
-        bulletcooldown = BULLET_COOLDOWN
+        bulletcooldown = ENEMYBULLET_COOLDOWN
         randomPos = pygame.Vector2(random.randint(0, 1280), -10)
         newBullet = Bullet(randomPos, player_pos)
-        enemyBulletList.append(newBullet)
-    
+        Enemybulletlist.append(newBullet)
+
+
+        
+          
+        
     
     
    
@@ -59,13 +66,19 @@ while running:
     player_pos.y += gravity * dt
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP] and canJump:
+    if keys[pygame.K_w] and canJump:
         gravity = -575
         canJump = False
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_a]:
         player_pos.x -= 300 * dt
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_d]:
         player_pos.x += 300 * dt
+    if keys[pygame.K_e] and playerbulletcooldown < 0: 
+        playerbulletcooldown = PLAYERBULLET_COOLDOWN
+        currentPosition = player_pos.copy()
+        newPlayerbullet = Bullet(currentPosition, (currentPosition.x+50, currentPosition.y))
+        playerbulletlist.append(newPlayerbullet)
+
 
     # Rebuild rect after movement so collisions use updated position
     player_rect = pygame.Rect(player_pos.x - 20, player_pos.y - 20, 40, 40)
@@ -118,7 +131,7 @@ while running:
                 platformList = levels[currentLevel-1]()
                 enemyList = enemies[currentLevel-1]()
                 gravity = 0
-                enemyBulletList.clear()
+                Enemybulletlist.clear()
             else:
                 running = False
             break
@@ -127,20 +140,46 @@ while running:
         enemy.update(dt)
         enemy.draw(screen)
         if player_rect.colliderect(enemy.rect):
-            playerHealth -= 2
+            playerHealth -= 5
+            enemyList.remove(enemy)
+        if enemy.health <= 0:
             enemyList.remove(enemy)
 
-    for bullet in enemyBulletList:
+
+    for bullet in playerbulletlist:
         bullet.update(dt)
-        bullet.draw(screen)
+        bullet.draw(screen,"blue")
+        playerbulletrect = pygame.Rect( bullet.pos.x - 10, bullet.pos.y - 10 , 20, 20)
+
+        if bullet.pos.x > 1280 or bullet.pos.x < 0:
+            print("PlayerBulletGone")
+            playerbulletlist.remove(bullet) 
+
+        for enemy in enemyList:
+            if playerbulletrect.colliderect(enemy.rect):
+                enemy.health -= 6
+                playerbulletlist.remove(bullet)
+
+        
+
+
+
+
+
+    for bullet in Enemybulletlist:
+        bullet.update(dt)
+        bullet.draw(screen, "red")
         bullet_rect = pygame.Rect( bullet.pos.x - 10, bullet.pos.y - 10 , 20, 20 )
         if bullet_rect.colliderect(player_rect):
             playerHealth -= 1
-            enemyBulletList.remove(bullet)
+            Enemybulletlist.remove(bullet)
         if bullet.pos.x >= 1280:
-            enemyBulletList.remove(bullet)
+            Enemybulletlist.remove(bullet)
         if bullet.pos.y >= 720:
-            enemyBulletList.remove(bullet)
+            Enemybulletlist.remove(bullet)
+
+
+
 
     if playerHealth <= 0:
         running = False
@@ -149,7 +188,7 @@ while running:
 
 
     pygame.draw.rect(screen,"red", (player_pos.x- 40 , player_pos.y- 60, 80, 10 ))
-    pygame.draw.rect(screen,"green", (player_pos.x- 40 , player_pos.y- 60, 80/10 * playerHealth, 10 ))
+    pygame.draw.rect(screen,"green", (player_pos.x- 40 , player_pos.y- 60, 80/20 * playerHealth, 10 ))
     # flip() the display to put your work on screen
     pygame.display.flip()
 
